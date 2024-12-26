f63d23cc05d0bbdd34a9692277acb933df26090c
Dan Collins <dan.collins.dev@gmail.com>
2024-12-26 16:06:39 -0500

    initial commit

---
+++ js/assignment.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const submissionBtn = document.getElementById("submission-btn");
+const smallestNumberElement = document.getElementById("smallest-number");
+const largestNumberElement = document.getElementById("largest-number");
+const averageNumberElement = document.getElementById("average-number");
+
+const myNumbers = [35, 130, 354, 100, 25, 134, 99, 200, 35];
